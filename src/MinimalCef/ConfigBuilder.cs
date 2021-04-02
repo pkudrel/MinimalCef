515df26f31c6b53e7cc37ac83cdaee80c50e95e6
Piotr Kudrel <piotr.kudrel@deneblab.com>
2021-04-02 13:40:17 +0200

    Update co chromium 88

--- src/MinimalCef/ConfigBuilder.cs
+++ src/MinimalCef/ConfigBuilder.cs
@@ -8,7 +8,7 @@ namespace MinimalCef
     {
         public static Config Create()
         {
-            var ret = new Config { PackageConfig = GetPackageConfig(), UseGlobalBrowserSubprocess = true };
+            var ret = new Config {PackageConfig = GetPackageConfig(), UseGlobalBrowserSubprocess = true};
             return ret;
         }
 
@@ -16,23 +16,29 @@ namespace MinimalCef
         {
             return new PackageConfig
             {
-                Name = "cefsharp_79.1.36_x64",
+                Name = "cefsharp_88.2.9_x64",
                 Nugets = new List<NugetInfo>
                 {
-                    new NugetInfo("CefSharp.Common", "79.1.360",
-                        new List<CopyInfo> {new CopyInfo("/CefSharp/x64", "/")}),
+                    new NugetInfo("CefSharp.Common", "88.2.90",
+                        new List<CopyInfo>
+                        {
+                            new CopyInfo("/CefSharp/x64", "/"),
+                            new CopyInfo("/lib/net452", "/"),
 
-                    new NugetInfo("CefSharp.WinForms", "79.1.360",
-                        new List<CopyInfo> {new CopyInfo("/CefSharp/x64", "/")}),
 
-                    new NugetInfo("cef.redist.x64", "79.1.36",
+                        }),
+
+                    new NugetInfo("CefSharp.WinForms", "88.2.90",
+                        new List<CopyInfo> {new CopyInfo("/lib/net452", "/")}),
+
+
+                    new NugetInfo("cef.redist.x64", "88.2.9",
                         new List<CopyInfo>
                         {
                             new CopyInfo("/CEF", "/"),
                             new CopyInfo("/CEF/locales", "/locales"),
-                            new CopyInfo("/CEF/swiftshader", "/swiftshader"),
-                        }),
-
+                            new CopyInfo("/CEF/swiftshader", "/swiftshader")
+                        })
                 }
             };
         }
